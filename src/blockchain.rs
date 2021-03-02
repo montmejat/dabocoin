2805e3e4b7add144568cff5a4da19719c4e5e5b6
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2021-03-02 23:20:06 +0100

    Added implementations for the data structs

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -14,10 +14,12 @@ pub mod block {
         pub time_stamp: String,
     }
 
-    pub fn get_hash(block: Block) -> u8 {
-        let mut hasher = Sha256::new();
-        hasher.update(block.id.as_bytes();
-        hasher.finalize()[..];
+    impl Block {
+        fn get_hash(&self) -> u8 {
+            let mut hasher = Sha256::new();
+            hasher.update(block.id.as_bytes();
+            hasher.finalize()[..];
+        }
     }
 }
 
@@ -26,16 +28,20 @@ pub mod chain {
         chain: [Block],
     }
 
-    pub fn get_last_block(chain: Chain) {
-        chain[chain.len() - 1]
-    }
+    impl Chain {
+        fn get_last_block(&self) -> Block {
+            &self.chain[&self.chain.len() - 1]
+        }
 
-    pub fn add_block(chain: Chain, transaction: Transaction, sender_public_key: String, signature: String) {
-        let block = Block {
-            id: "todo: generate id"
-            previous_hash: chain.get_last_block()
-            transaction: transaction,
-            time_stamp: "todo: get current date"
+        fn add_block(&self, transaction: Transaction, sender_public_key: String, signature: String) {
+            let block = Block {
+                id: "todo: generate id"
+                previous_hash: chain.get_last_block()
+                transaction: transaction,
+                time_stamp: "todo: get current date"
+            }
+            
+            &self.chain.push(block);
         }
     }
 }
@@ -49,17 +55,19 @@ pub mod wallet {
         pub private_key: String,
     }
 
-    pub fn create_wallet() -> Wallet {
-        let mut rng = OsRng;
-        let bits = 2048;
+    impl Wallet {
+        fn new() -> Wallet {
+            let mut rng = OsRng;
+            let bits = 2048;
 
-        Wallet {
-            public_key: RSAPublicKey::from(&priv_key),
-            private_key: RSAPrivateKey::new(&mut rng, bits).expect("failed to generate a key"),
+            Wallet {
+                public_key: RSAPublicKey::from(&priv_key),
+                private_key: RSAPrivateKey::new(&mut rng, bits).expect("failed to generate a key"),
+            }
         }
-    }
 
-    pub fn send_money() {
-        // todo
+        pub fn send_money() {
+            // todo
+        }
     }
 }
